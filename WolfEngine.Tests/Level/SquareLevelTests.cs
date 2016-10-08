0a8e1506940b4ea77935fb562c3b01e9b467fd83
Jon Seaman <jonmseaman@live.com>
2016-10-08 19:39:53 -0400

    Removed ILevelTests
        They really just don't seem useful.
        Right now, there is no specification that we must have any location
        accessible, so it is hard to test the get methods that use a
        location.

--- WolfEngine.Tests/Level/SquareLevelTests.cs
+++ WolfEngine.Tests/Level/SquareLevelTests.cs
@@ -1,9 +1,10 @@
-﻿using WolfEngine.Level;
+﻿using WolfEngine.Entiity;
+using WolfEngine.Level;
 using Xunit;
 
 namespace WolfEngine.Tests.Level
 {
-    public class SquareLevelTests : ILevelTests
+    public class SquareLevelTests
     {
         [Theory]
         [InlineData(1)]
@@ -14,5 +15,20 @@ namespace WolfEngine.Tests.Level
             var level = new SquareLevel(width);
             Assert.Equal(width, level.LevelWidth);
         }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(256, 0)]
+        [InlineData(0, 256)]
+        public void TestCreaturesAddFromEmpty(int x, int y)
+        {
+            var level = new SquareLevel(256);
+            var l = new Location(x, y);
+            level.Creatures[l].Add(new Creature());
+
+            Assert.NotNull(level.Creatures[l]);
+            Assert.Equal(1, level.Creatures[l].Count);
+        }
+
     }
 }
