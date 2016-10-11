09f82810eac6beb5c72521919bc37e19638752a6
Jon Seaman <jonmseaman@live.com>
2016-10-11 15:03:26 -0400

    Comments on attributes.

--- WolfEngine/Entiity/ItemAttributes.cs
+++ WolfEngine/Entiity/ItemAttributes.cs
@@ -4,7 +4,14 @@ namespace WolfEngine.Entiity
 {
     public class ItemAttributes : Attributes
     {
+        /// <summary>
+        /// The damage per attack.
+        /// </summary>
         public int Attack { get; set; }
+
+        /// <summary>
+        /// The time between attacks.
+        /// </summary>
         public TimeSpan AttackTime { get; set; }
     }
 }
