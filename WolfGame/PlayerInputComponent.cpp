96423779f71ab89858ce19d61ae1470d5d2219c8
Jon Seaman <jonmseaman@live.com>
2016-12-18 10:36:32 -0500

    Handled some input from player

--- WolfGame/PlayerInputComponent.cpp
+++ WolfGame/PlayerInputComponent.cpp
@@ -3,11 +3,34 @@
 #include <curses.h>
 
 using namespace WolfEngine::Entiity;
+using namespace WolfEngine::Level;
 
-PlayerInputComponent::PlayerInputComponent() {
-}
+PlayerInputComponent::PlayerInputComponent() {}
 
-void PlayerInputComponent::Update(Creature^ c)
-{
-	// TODO: Implement PlayerInputComponenet::Update
+void PlayerInputComponent::Update(Creature^ c) {
+	
+	int input = getch();
+	switch (input) {
+		case 'W':
+		case 'w':
+			c->Move(Direction::North);
+			System::Console::WriteLine(L"Default for PlayerInputComponentUpdate");
+
+			break;
+		case 'A':
+		case 'a':
+			c->Move(Direction::West);
+			break;
+		case 'S':
+		case 's':
+			c->Move(Direction::South);
+			break;
+		case 'D':
+		case 'd':
+			c->Move(Direction::East);
+			break;
+		case ERR:
+		default:
+			break;
+	}
 }
