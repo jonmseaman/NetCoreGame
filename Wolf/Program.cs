64464add29e8d00a0c3a702d94d34717be9f7c4a
Jon Seaman <jonmseaman@live.com>
2017-02-03 16:04:17 -0500

    Added Wolf project

---
+++ Wolf/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wolf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
