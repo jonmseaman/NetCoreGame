60db016f6e2c4164e6aeec2d3e06c134eaa441b3
Jon Seaman <jonmseaman@live.com>
2017-03-11 13:55:50 -0500

    Added player class

---
+++ MPEngine/Entity/Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPEngine.Entity
+{
+    public class Player : Creature
+    {
+        public string Name { get; set; }
+    }
+}
