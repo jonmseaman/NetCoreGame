02f65bfce40c4570f3c9ff7f68f3067d7c4add34
Jon Seaman <jonmseaman@live.com>
2017-03-12 20:40:11 -0400

    Improve publish script

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,10 @@
 dotnet restore
 dotnet build
 
-dotnet publish Game -c Release -f netcoreapp1.1 -r win10-x64
-dotnet publish Game -c Release -f netcoreapp1.1 -r win7-x64
-dotnet publish Game -c Release -f netcoreapp1.1 -r ubuntu.16.04-x64
+$runtimes = "win10-x64", "win7-x64", "ubuntu.16.04-x64"
+
+New-Item -ItemType Directory -Force -Path Bin\publish\
+foreach ($r in $runtimes) {
+    dotnet publish Game -c Release -f netcoreapp1.1 -r $r
+    Compress-Archive -Force ".\Game\bin\Release\netcoreapp1.1\${r}\publish\*" "Bin\publish\${r}.zip"
+}
