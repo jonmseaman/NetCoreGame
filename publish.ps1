8a3711460051e0e1dcc892b4f95e1eb7068142f7
Jon Seaman <jonmseaman@live.com>
2017-10-16 18:58:19 -0400

    Fix runtimes and update publish script

--- publish.ps1
+++ publish.ps1
@@ -1,12 +1,12 @@
 dotnet build
 
-# $runtimes = "win10-x64", "win7-x64", "ubuntu.16.04-x64"
+# $runtimes = "win10-x64", "win7-x64", "linux-x64"
 $runtimes = "win10-x64"
 
 New-Item -ItemType Directory -Force -Path Bin\publish\
 foreach ($r in $runtimes) {
-    dotnet publish Game -c Release -f netcoreapp1.1 -r $r
-    Compress-Archive -Force ".\Game\bin\Release\netcoreapp1.1\${r}\publish\*" "Bin\publish\${r}.zip"
+    dotnet publish Game -c Release -f netcoreapp2.0 -r $r
+    Compress-Archive -Force ".\Game\bin\Release\netcoreapp2.0\${r}\publish\*" "Bin\publish\${r}.zip"
 }
 
 echo "Success"
